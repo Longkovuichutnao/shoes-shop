021eb5dfe822be6ed8eba03842aa5c17eba947b3
Khanh <ndk2204@gmail.com>
2023-06-23 15:06:08 +0700

    cart v4

--- controller/cart.js
+++ controller/cart.js
@@ -26,17 +26,10 @@ window.onload = function () {
             id: result.data.content.id,
             image: result.data.content.image,
             name: result.data.content.name,
+            size: result.data.content.size,
             price: result.data.content.price,
             quantity: 1,
         }
-
-        dsShoes.arrShoe.map(function (ktraID, index) {
-            if (ktraID.id === shoe.id) {
-                dsShoes.arrShoe[index].quantity += 1
-                setLocalStorage(dsShoes.arrShoe);
-            }
-        })
-
         if (dsShoes.arrShoe.every(function (ktraID, index) {
             return ktraID.id !== shoe.id;
         })) {
@@ -48,37 +41,70 @@ window.onload = function () {
         console.log(error);
     })
 }
-
 //Hiển thị SP =======================================================
 function hienThiSP(mangLocal) {
     let content = "";
-    let totalList = 0;
     mangLocal.map(function (shoe, index) {
+        let sizeList = "";
+        shoe.size.map(function (shoeSize, index) {
+            let infoSize = `
+            <option>${shoeSize}</option>
+            `
+            sizeList += infoSize
+        })
         let total = shoe.price * shoe.quantity;
         let infoSP = `<tr class="table-list">
-        <td class="check"><input type="checkbox"></td>
+        <td class="check"><input id="check${shoe.id}" onclick="check()" type="checkbox"></td>
         <td class="shoeID">${shoe.id}</td>
         <td class="shoeIMG"><img class="img-fluid" src= ${shoe.image}></td>
         <td class="shoeName">${shoe.name}</td>
+        <td class="shoeSize"><select class="form-control-sm">${sizeList}
+        </select></td>
         <td class="shoePrice">${shoe.price}$</td>
         <td class="quantity">
             <button class="btn btn-dark" onclick="clickDown(${shoe.id})" >
                 <i class="fa-solid fa-minus"></i></button>
-                <span id="quantityClick">${shoe.quantity}</span>
+                <span id="quantityClick${shoe.id}">${shoe.quantity}</span>
             <button class="btn btn-dark" onclick="clickUp(${shoe.id})" >
                 <i class="fa-solid fa-plus"></i></button>
         </td>
-        <td class="total"> ${total.toLocaleString('en-US')}$</td>
+        <td class="total" id="total${shoe.id}"> ${total.toLocaleString('en-US')}$</td>
         <td class="select">
        <!-- <button class="btn btn-info" onclick="editSP(${shoe.id})">Edit</button> -->
             <button class="btn btn-danger" onclick="xoaSP(${shoe.id})">Delete</button>
         </td>
         </tr>`
         content += infoSP
-        totalList += total
     })
     document.getElementById("tableDanhSach").innerHTML = content;
-    document.getElementById("total-price").innerHTML = totalList.toLocaleString('en-US') + '$';
+    document.getElementById("total-price").innerHTML = 0 + '$';
+}
+
+//Click CheckBox  =======================================================
+function check() {
+    let tong = 0;
+    dsShoes.arrShoe.map(function (shoe, index) {
+        let timcheck = document.getElementById('check' + shoe.id)
+        let total = 0;
+        if (timcheck.checked == true) {
+            total = dsShoes.arrShoe[index].quantity * dsShoes.arrShoe[index].price
+        }
+        tong += total;
+    })
+    document.getElementById("total-price").innerHTML = tong.toLocaleString('en-US') + '$';
+}
+function checkAll() {
+    let checkAll = document.getElementById('checkAll');
+    dsShoes.arrShoe.map(function (shoe, index) {
+        let timcheck = document.getElementById('check' + shoe.id)
+        if (checkAll.checked == true) {
+            timcheck.checked = true;
+            check();
+        } else {
+            timcheck.checked = false;
+            check();
+        }
+    })
 }
 
 //Click Delete  =======================================================
@@ -90,19 +116,43 @@ function xoaSP(shoeID) {
 
 //Click + -  =======================================================
 function clickUp(shoeID) {
+    document.getElementById('checkAll').checked = true
+    document.getElementById('check' + shoeID).checked = true
     let i = dsShoes.timIndex(shoeID)
     dsShoes.arrShoe[i].quantity += 1
-    hienThiSP(dsShoes.arrShoe);
+    let total = dsShoes.arrShoe[i].quantity * dsShoes.arrShoe[i].price
+    document.getElementById("quantityClick" + shoeID).innerHTML = dsShoes.arrShoe[i].quantity;
+    document.getElementById("total" + shoeID).innerHTML = total.toLocaleString('en-US') + '$';
     setLocalStorage(dsShoes.arrShoe);
+    check();
 }
 
 function clickDown(shoeID) {
+    document.getElementById('checkAll').checked = true
+    document.getElementById('check' + shoeID).checked = true
     let i = dsShoes.timIndex(shoeID)
     if (dsShoes.arrShoe[i].quantity > 0) {
         dsShoes.arrShoe[i].quantity -= 1
-        hienThiSP(dsShoes.arrShoe);
+        let total = dsShoes.arrShoe[i].quantity * dsShoes.arrShoe[i].price
+        document.getElementById("quantityClick" + shoeID).innerHTML = dsShoes.arrShoe[i].quantity;
+        document.getElementById("total" + shoeID).innerHTML = total.toLocaleString('en-US') + '$';
         setLocalStorage(dsShoes.arrShoe);
+        check();
     } else {
         alert("Tăng số lượng sản phẩm để mua bạn ơi!")
+    }
+}
+
+//Click oder  =======================================================
+function order() {
+    if (dsShoes.arrShoe.some(function (shoe, index) {
+        let timcheck = document.getElementById('check' + shoe.id)
+        return timcheck.checked == true
+    })) {
+        alert("Thanh toán thành công!")
+        localStorage.clear();
+        window.location = "../index.html"
+    } else {
+        alert("Vui lòng lựa chọn sản phẩm để thanh toán!")
     }
 }
